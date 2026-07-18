2d389f3695b473d60f734a3dad02610d78d40f24
Snehal <snehudatir12@gmail.com>
2026-07-18 11:12:05 +0000

    variables added

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -1,24 +1,24 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "ntms-rg"
-  location = "East US"
+  name = "${var.environment}-${var.business_unit}snehal-ntms-rg"
+  location = var.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "ntms-vnet"
+  name                = "${var.environment}-${var.business_unit}snehal-ntms-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "ntms-snet"
+  name                 = "${var.environment}-${var.business_unit}-${var.subnet_name}"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_security_group" "nsg" {
-  name                = "ntms-nsg"
+  name                = "${var.environment}-${var.business_unit}snehal-ntms-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
